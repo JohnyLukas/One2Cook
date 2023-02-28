1c07b76615084352997f0caef1d692b95a4e174e
Evgeniy Lukyanchikov <evgenij.luk11@gmail.com>
2023-02-28 17:39:53 +0600

    Fixes

--- app/src/main/java/com/example/one2cook/domain/model/Next.kt
+++ app/src/main/java/com/example/one2cook/domain/model/Next.kt
@@ -1,13 +1,11 @@
 package com.example.one2cook.domain.model
 
 import android.os.Parcelable
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
 @Parcelize
 data class Next(
-    @Json(name = "href")
     val nextPageUrl: String?
 ) : Parcelable
